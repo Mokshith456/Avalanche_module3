7b38c4f03232aa0ee69b94685596fa1d16f18c51
Mokshith456 <110161392+Mokshith456@users.noreply.github.com>
2023-11-12 20:43:55 +0530

    Add files via upload

---
+++ Avalanche_module3.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+
+contract MOK47 is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("MOK47", "MOK")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 1 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
